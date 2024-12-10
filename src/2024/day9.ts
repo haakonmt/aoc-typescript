e22512de1ede5b780f116e16f3eb1807a431af2b
Håkon Meyer Tørnquist <haakon.t@gmail.com>
2024-12-10 20:47:01 +0100

    :sparkles: Day 9, part 2

--- src/2024/day9.ts
+++ src/2024/day9.ts
@@ -1,39 +1,91 @@
+import { arr } from '../utils.ts'
+
+interface File {
+  id: number
+  size: number
+  blocks: number[]
+}
+
+function prepare(lines: string[]) {
+  const blocks: (number | undefined)[] = []
+  const availableBlocks: number[] = []
+
+  const input = lines[0].split('').map(Number)
+  const files: File[] = []
+
+  let currentIndex = 0
+  for (let i = 0; i < input.length; i++) {
+    const id = Math.floor(i / 2)
+    let num = input[i]
+    const file: File = { id, size: num, blocks: [] }
+    while (num > 0) {
+      if (i % 2 === 0) {
+        blocks[currentIndex] = id
+        file.blocks.push(currentIndex)
+      } else {
+        availableBlocks.push(currentIndex)
+      }
+      num--
+      currentIndex++
+    }
+
+    if (i % 2 === 0) {
+      files.push(file)
+    }
+  }
+
+  return {
+    files,
+    blocks,
+    availableBlocks,
+  }
+}
+
+function checksum(values: Array<number | undefined>) {
+  return values
+    .reduce<number>((sum, curr, i) => sum + ((curr ?? 0) * i), 0)
+}
+
 export default {
   part1({ lines }) {
-    const values: number[] = []
-    const availableIndices: number[] = []
+    const { blocks, availableBlocks } = prepare(lines)
 
-    const nums = lines[0].split('').map(Number)
-
-    let currentIndex = 0
-    for (let i = 0; i < nums.length; i++) {
-      const id = Math.floor(i / 2)
-      let num = nums[i]
-      while (num > 0) {
-        if (i % 2 === 0) {
-          values[currentIndex] = id
-        } else {
-          availableIndices.push(currentIndex)
-        }
-        num--
-        currentIndex++
+    while (availableBlocks.length) {
+      const index = availableBlocks.shift()!
+      let block = blocks.pop()
+      while (!block) {
+        block = blocks.pop()
       }
+      blocks[index] = block
     }
 
-    while (availableIndices.length) {
-      const index = availableIndices.shift()!
-      let value = values.pop()
-      while (!value) {
-        value = values.pop()
-      }
-      values[index] = value
-    }
-
-    return values
-      .filter((it) => it !== undefined)
-      .reduce((sum, curr, i) => sum + curr * i, 0)
+    return checksum(blocks)
   },
   part2({ lines }) {
-    return lines.length
+    const { blocks, files } = prepare(lines)
+
+    while (files.length) {
+      const file = files.pop()!
+
+      let index = arr.windowed(blocks, file.size).findIndex(
+        (it) => it.every((value) => typeof value !== 'number'),
+      )
+
+      if (index === -1 || index >= file.blocks[0]) {
+        continue
+      }
+
+      for (const i of file.blocks) {
+        delete blocks[i]
+      }
+
+      while (file.size) {
+        blocks[index] = file.id
+        index++
+        file.size--
+      }
+    }
+
+    return checksum(blocks)
   },
 } satisfies Day
